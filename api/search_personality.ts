b7a02a443bed6d7178a8fc505752235d4cf90797
kimminam <bluemind8520@gmail.com>
2024-05-27 11:34:44 +0900

    Feat : 자료가 없는 것을 검색했을 때 처리 및 VecelReqQuery Import문 삭제

--- api/search_personality.ts
+++ api/search_personality.ts
@@ -1,24 +1,22 @@
-import type {
-  VercelRequest,
-  VercelRequestQuery,
-  VercelResponse,
-} from '@vercel/node'
-import _personality_list from './_personality_list'
+import type { VercelRequest, VercelResponse } from '@vercel/node';
+import _personality_list from './_personality_list';
 
 export default function handler(req: VercelRequest, res: VercelResponse) {
-  const persnalityValueList: Array<string> = Object.values(_personality_list)
-  let { value } = req.query
-  console.log(value)
+  const persnalityValueList: Array<string> = Object.values(_personality_list);
+  let { value } = req.query;
+  console.log(value);
 
   if (Array.isArray(value)) {
-    value = value[0]
+    value = value[0];
   }
 
   if (!value) {
-    res.status(404).json('value not found!')
+    res.status(404).json('value not found!');
   }
 
   if (persnalityValueList.includes(value)) {
-    res.status(200).json(value)
+    res.status(200).json(value);
+  } else {
+    res.status(404).json('Not Value, 죄송해요! 자료가 없어요!');
   }
 }
